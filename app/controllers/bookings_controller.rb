fd65631d1f7adf753fd8e797b473bb80b0a591e5
Joao Novis <novis.joao@gmail.com>
2023-11-22 12:02:33 +0100

    bookings index page done (missing delete button)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where("user_id = ? AND end_time >= ?", current_user.id, Time.now).order(start_time: :asc)
+    @past_bookings = Booking.where("user_id = ? AND end_time < ?", current_user.id, Time.now).order(start_time: :asc)
+  end
 
   def create
     @booking = Booking.new(booking_params)
@@ -11,7 +15,7 @@ class BookingsController < ApplicationController
       @days = (@booking.end_time - @booking.start_time) / (60 * 60 * 24)
       @booking.total_price = @bike.price_per_day * @days
       @booking.save
-      redirect_to bike_path(@bike)
+      redirect_to bookings_path
     else
       render "bikes/show", status: :unprocessable_entity
     end
